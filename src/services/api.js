7610e15bdcc241851566684e56e6b0fca223c718
Prashant-kumar-pcpk <prashantkumar.pcpk@gmail.com>
2026-08-26 01:36:15 +0530

    update backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://be-erp-examination-engine.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
